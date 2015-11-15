209a3f81628bc1e467f5f1ef6a1febcefc9b5a30
Mer Alba <malbamoyano@gmail.com>
2015-11-15 19:42:53 +0100

    videos

--- Practica2/MainWindow.xaml.cs
+++ Practica2/MainWindow.xaml.cs
@@ -110,14 +110,27 @@ namespace Posturas
         /// </summary>
         private DrawingImage imageSource;
 
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        private WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// Intermediate storage for the depth data received from the camera
+        /// </summary>
+        private DepthImagePixel[] depthPixels;
+
+        /// <summary>
+        /// Intermediate storage for the depth data converted to color
+        /// </summary>
+        private byte[] colorPixels;
 
         //Creaccion de las coordenadas donde deberan dibujarse las visualizaciones
         Point puntocomp;
 
-        private bool visible = false, correcto = false, recto = false, posic1 = false, posic2 = false;
-        // Estructura para indicar las coordenadas de un punto.
+        private bool visible = false, correcto = false, recto = false, posic1 = false, posic2 = false, infrared=false;
 
-        private int cont = 0;
+        private int contadorCaptura = 0, contadorVideo=0, contadorDepth=0, contadorInfrared=0;
 
         //Variable para cambiar la precisión
         private float precision = 4;
@@ -178,8 +191,6 @@ namespace Posturas
         /// <param name="e"></param>
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
-
-
        
             // Create the drawing group we'll use for drawing
             this.drawingGroup = new DrawingGroup();
@@ -213,8 +224,6 @@ namespace Posturas
                 // Turn on the skeleton stream to receive skeleton frames
                 this.sensor.SkeletonStream.Enable();
 
-
-
                 //Color stream
                 this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
 
@@ -223,7 +232,7 @@ namespace Posturas
 
                 this.sensor.ColorFrameReady += this.SensorColorFrameReady;
 
-
+                this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
 
                 // Start the sensor!
                 try
@@ -363,8 +372,22 @@ namespace Posturas
 
                         Point pr = SkeletonPointToScreen(skeleton.Joints[JointType.HandRight].Position);
                         Point pl = SkeletonPointToScreen(skeleton.Joints[JointType.HandLeft].Position);
-                        if (tocarCaptura(pr, pl,puntocomp))
+                        if (tocarCaptura(pr, pl, puntocomp) && contadorCaptura >= 50)
+                        {
                             this.textblock.Text = "Captura";
+                        }
+                        if (tocarVideo(pr, pl, puntocomp) && contadorVideo >= 50)
+                        {
+                            this.textblock.Text = "Captura";
+                        }
+                        if (tocarDepth(pr, pl, puntocomp) && contadorDepth >= 50)
+                        {
+                            this.textblock.Text = "Captura";
+                        }
+                        if (tocarInfrared(pr, pl, puntocomp) && contadorInfrared >= 50)
+                        {
+                            this.textblock.Text = "Captura";
+                        }
                         else
                         {
                             //this.textblock.Text = .ToString() + "\n" + handRight.y.ToString();
@@ -382,7 +405,7 @@ namespace Posturas
                     dc.DrawEllipse(Brushes.Green, pen, puntocomp, 40, 40);
 
 
-                    if (skeletons.Length != 0)
+                  /*  if (skeletons.Length != 0)
                     {
                         foreach (Skeleton skel in skeletons)
                         {
@@ -404,7 +427,7 @@ namespace Posturas
                             }
                         }
                     }
-
+                    */
                     // prevent drawing outside of our render area
                     this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
                 }
@@ -428,8 +451,91 @@ namespace Posturas
                     sensorVideo.Source = BitmapSource.Create(colorFrame.Width, colorFrame.Height, 96, 96, PixelFormats.Bgr32, null, colorData, colorFrame.Width * colorFrame.BytesPerPixel);
                 }
 
-
         }
+        // <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null && !infrared)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    // Get the min and max reliable depth for the current frame
+                    int minDepth = depthFrame.MinDepth;
+                    int maxDepth = depthFrame.MaxDepth;
+
+                    // Convert the depth to RGB
+                    int colorPixelIndex = 0;
+                    for (int i = 0; i < this.depthPixels.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = depthPixels[i].Depth;
+
+                        // To convert to a byte, we're discarding the most-significant
+                        // rather than least-significant bits.
+                        // We're preserving detail, although the intensity will "wrap."
+                        // Values outside the reliable depth range are mapped to 0 (black).
+
+                        // Note: Using conditionals in this loop could degrade performance.
+                        // Consider using a lookup table instead when writing production code.
+                        // See the KinectDepthViewer class used by the KinectExplorer sample
+                        // for a lookup table example.
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                        // Write out blue byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out green byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out red byte                        
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                        // If we were outputting BGRA, we would write alpha here.
+                        ++colorPixelIndex;
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's InfraredFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorInfraredFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null && infrared )
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * colorFrame.BytesPerPixel,
+                        0);
+                }
+            }
+        }
+
 
         #region Deteccion de postura
 
@@ -460,15 +566,78 @@ namespace Posturas
         {
            bool post = false;
 
-           if ((Math.Abs(handRight.Y - comp.Y) < 10*precision && Math.Abs(handRight.X - comp.X) < 10*precision) ||
-               (Math.Abs(handLeft.Y - comp.Y) < 10*precision && Math.Abs(handLeft.X - comp.X) < 10*precision))
-            {
+           if ((Math.Abs(handRight.Y - comp.Y) < 10 * precision && Math.Abs(handRight.X - comp.X) < 10 * precision) ||
+               (Math.Abs(handLeft.Y - comp.Y) < 10 * precision && Math.Abs(handLeft.X - comp.X) < 10 * precision))
+           {
 
-                post = true;
-            }
+               post = true;
+               contadorCaptura++;
+           }
+           else
+           {
+               contadorCaptura = 0;
+           }
             
             return post;
         }
+
+        bool tocarVideo(Point handRight, Point handLeft, Point comp)
+        {
+            bool post = false;
+
+            if ((Math.Abs(handRight.Y - comp.Y) < 10 * precision && Math.Abs(handRight.X - comp.X) < 10 * precision) ||
+                (Math.Abs(handLeft.Y - comp.Y) < 10 * precision && Math.Abs(handLeft.X - comp.X) < 10 * precision))
+            {
+
+                post = true;
+                contadorVideo++;
+            }
+            else
+            {
+                contadorVideo = 0;
+            }
+
+            return post;
+        }
+        bool tocarDepth(Point handRight, Point handLeft, Point comp)
+        {
+            bool post = false;
+
+            if ((Math.Abs(handRight.Y - comp.Y) < 10 * precision && Math.Abs(handRight.X - comp.X) < 10 * precision) ||
+                (Math.Abs(handLeft.Y - comp.Y) < 10 * precision && Math.Abs(handLeft.X - comp.X) < 10 * precision))
+            {
+
+                post = true;
+                contadorDepth++;
+            }
+            else
+            {
+                contadorDepth = 0;
+            }
+
+            return post;
+        }
+        bool tocarInfrared(Point handRight, Point handLeft, Point comp)
+        {
+            bool post = false;
+
+            if ((Math.Abs(handRight.Y - comp.Y) < 10 * precision && Math.Abs(handRight.X - comp.X) < 10 * precision) ||
+                (Math.Abs(handLeft.Y - comp.Y) < 10 * precision && Math.Abs(handLeft.X - comp.X) < 10 * precision))
+            {
+
+                post = true;
+                contadorInfrared++;
+            }
+            else
+            {
+                contadorInfrared = 0;
+            }
+
+            return post;
+        }
+
+
+
 
         //Controlador del gesto.
         /*bool Posic2(Puntos handRight, Puntos handLeft)
@@ -644,14 +813,85 @@ namespace Posturas
             }
         }
 
+        private void cambioVisualizacion(int cont)
+        {
 
-        private void btnCaptureImage_Click(object sender, RoutedEventArgs e)
+            if (cont%3==0)
+            {
+                this.sensor.ColorStream.Disable();
+                this.grid.Children.Remove(this.sensorVideo);
+                this.grid.Children.Remove(this.sensorDepth);
+                this.grid.Children.Remove(this.sensorInfrared);
+                // Turn on the depth stream to receive depth frames
+                this.grid.Children.Insert(0, this.sensorDepth);
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.sensorDepth.Source = this.colorBitmap;
+            }
+            if(cont%3==1)
+            {
+                infrared = true;
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Disable();
+                this.grid.Children.Remove(this.sensorDepth);
+
+                this.grid.Children.Insert(0, this.sensorInfrared);
+
+                // Turn on the color stream to receive color frames
+
+
+                this.sensor.ColorFrameReady -= this.SensorColorFrameReady;
+                this.sensor.ColorFrameReady += this.SensorInfraredFrameReady;
+                this.sensor.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+
+                // Allocate space to put the pixels we'll receive
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Gray16, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.sensorInfrared.Source = this.colorBitmap;
+                
+            }
+            if (cont % 3 == 2)
+            {
+                infrared = false;
+                
+                this.sensor.ColorFrameReady -= this.SensorInfraredFrameReady;
+                this.sensor.ColorStream.Disable();
+
+                this.grid.Children.Remove(this.sensorInfrared);
+
+                this.grid.Children.Insert(0, this.sensorVideo);
+
+                this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                 
+            }
+
+            cont++;
+
+        }
+
+
+       /* private void btnCaptureImage_Click(object sender, RoutedEventArgs e)
         {
             BitmapSource image = (BitmapSource)Image.Source;
 
-            image.
+            
         }
-
+        */
 
     }
 }
